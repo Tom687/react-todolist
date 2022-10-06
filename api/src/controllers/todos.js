03ae6929c01cfa25e8cd8bd7af6d9d218fabd4db
Tom687 <tom.pomarede687@gmail.com>
2022-10-06 21:45:33 +0200

    Made use of deserializeUser() and use res.locals.user.id instead of Redis sessions (req.session.user.id)

--- api/src/controllers/todos.js
+++ api/src/controllers/todos.js
@@ -5,10 +5,10 @@ import moment from 'moment';
 
 export const insertTodo = catchAsync(async (req, res, next) => {
 	const { title, completed } = req.body;
-
+	
 	if (!title || title === 'e')
 		return next(new AppError('Il manque des infos pour insérer le todo en DB', 400));
-	
+
 	let status, doneOn;
 	if (completed === true) {
 		status = 'fait';
@@ -18,15 +18,15 @@ export const insertTodo = catchAsync(async (req, res, next) => {
 		status = 'non';
 		doneOn = undefined;
 	}
-	
+
 	const [insertedTodo] = await db('todos').insert({
-		id_member: req.session.user.id,
+		id_member: res.locals.user.id,
 		title,
 		status,
 		created_on: moment(), // TODO : Voir si moment() donne la bonne heure ? Utiliser locale ?
 		done_on: doneOn,
 	}, ['id']);
-	
+
 	res.status(201).json({
 		status: 'success',
 		message: 'Todo créé et inséré en DB avec succès',
@@ -36,7 +36,7 @@ export const insertTodo = catchAsync(async (req, res, next) => {
 
 export const getTodos = catchAsync(async (req, res, next) => {
 	const todos = await db('todos').select('id', 'id_member AS memberId', 'title', 'status', 'created_on AS createdOn', 'done_on AS doneOn')
-		.where({ id_member: req.session.user.id })
+		.where({ id_member: res.locals.user.id })
 		.orderBy('id', 'asc'); // TODO : Pour l'ordre, utiliser un champ "position" ?
 	
 	res.status(200).json({
