92ea7aa2d65824f89a5d265a037911a8e04aae79
Tom687 <tom.pomarede687@gmail.com>
2022-10-12 01:33:58 +0200

    First refactor to styled-components

--- client/src/components/CustomCheckbox/CustomCheckbox.js
+++ client/src/components/CustomCheckbox/CustomCheckbox.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import clsx from 'clsx';
-
-import './CustomCheckbox.css';
+import CheckIcon from './checkmark2.png';
+import styled from 'styled-components';
 
 const CustomCheckbox = ({ onClick, done }) => {
 	const [isChecked, setIsChecked] = useState(false);
@@ -19,22 +18,19 @@ const CustomCheckbox = ({ onClick, done }) => {
 		}
 	}
 	return (
-		<div
-			className={clsx('custom-checkbox-container')}
+		<Wrapper
 			onClick={onClick}
 		>
-			<input
+			<Checkbox
 				type="checkbox"
 				checked={isChecked}
 				onChange={() => onChange()}
-				className={clsx('custom-checkbox')}
 			/>
-			<span
-				// TODO : Voir meilleur moyen pour le .checked ?
-				className={clsx('custom-visible-checkbox', { 'checked': isChecked || done })}
+			<Checkmark
+				isChecked={isChecked || done}
 				onClick={() => onChange()}
-			></span>
-		</div>
+			></Checkmark>
+		</Wrapper>
 	);
 };
 
@@ -42,5 +38,70 @@ CustomCheckbox.propTypes = {
 	onClick: PropTypes.func.isRequired,
 	done: PropTypes.bool.isRequired,
 };
+
+const Wrapper = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: 1.5rem;
+  cursor: pointer;
+
+  /* TODO : Pour éviter la sélection du text quand double click sur le checkbox */
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none;
+  /* Non-prefixed version, currently
+																	 supported by Chrome, Edge, Opera and Firefox */
+  /*transition: 1s;*/
+
+  /* TODO NEW */
+  display: flex;
+  align-items: center;
+`;
+
+const Checkbox = styled.input`
+  border: 0;
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  white-space: nowrap;
+  width: 1px;
+`;
+
+const Checkmark = styled.span`
+  display: inline-block;
+  width: 2rem;
+  height: 2rem;
+  background: white;
+  border-radius: 50%;
+  border: 1px solid #e6e6e6;
+  vertical-align: sub;
+ 
+	${props => props.isChecked} {
+    border-color: rgba(186, 224, 189, 0.75);
+		
+    &::before {
+      display: block;
+      content: '';
+      position: relative;
+      bottom: 5px;
+      left: 2px;
+      /*background: url('../../assets/img/checkmark2.png');*/
+      /*background: url('./checkmark2.png');*/
+      background: url(${CheckIcon});
+      background-position: center;
+      background-size: cover;
+      background-repeat: no-repeat;
+      width: 2.25rem;
+      height: 2.25rem;
+    }
+	}
+`;
 
 export default CustomCheckbox;
