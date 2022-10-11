f2a6d9a0f87aadafda01de50be2900c412665a1a
Tom687 <tom.pomarede687@gmail.com>
2022-10-12 01:48:40 +0200

    Refactored to styled-components. FIXME : Outline of the buttons set to 0 because of previous css config in todos.css

--- client/src/features/TodoList/Filter/FilterLink.js
+++ client/src/features/TodoList/Filter/FilterLink.js
@@ -3,15 +3,17 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
 import './FilterLink.css';
+import styled from 'styled-components';
 
 const FilterLink = ({ filter, onClick, activeFilter, children }) => {
 	return (
-		<button
+		<Button
 			onClick={onClick}
-			className={clsx('filter-link', { 'active': activeFilter === filter })}
+			//className={clsx('filter-link', { 'active': activeFilter === filter })}
+			isActive={activeFilter === filter}
 		>
 			{ children }
-		</button>
+		</Button>
 	);
 };
 
@@ -29,5 +31,27 @@ FilterLink.propTypes = {
 	onClick: PropTypes.func.isRequired,
 	children: PropTypes.string.isRequired,
 };
+
+const Button = styled.button`
+  cursor: pointer;
+  font-size: 1rem;
+  padding: 0.1rem 0.25rem;
+  border: 1px solid transparent;
+  border-radius: 3px;
+  margin: 0 0.2rem;
+	
+	&:hover {
+    border: 1px solid #cc9a9a;
+	}
+
+	${props => props.isActive} {
+    border: 1px solid #af5b5e;
+	}
+
+  &:focus {
+    outline: none;
+    background-color: rgba(175, 91, 94, 0.3);
+  }
+`;
 
 export default FilterLink;
