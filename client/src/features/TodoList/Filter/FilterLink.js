26e3189de427ec23f2aa6e959ae184162a1fe376
Tom687 <tom.pomarede687@gmail.com>
2022-10-12 02:57:13 +0200

    Typo

--- client/src/features/TodoList/Filter/FilterLink.js
+++ client/src/features/TodoList/Filter/FilterLink.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import clsx from 'clsx';
-
-import './FilterLink.css';
 import styled from 'styled-components';
 
 const FilterLink = ({ filter, onClick, activeFilter, children }) => {
