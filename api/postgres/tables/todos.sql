40a11ae4cb0f9c769ff59717254a19dbeeb1d399
Tom687 <tom.pomarede687@gmail.com>
2022-10-14 00:14:02 +0200

    Changed 'status' ENUM to 'completed' BOOL

--- api/postgres/tables/todos.sql
+++ api/postgres/tables/todos.sql
@@ -1,12 +1,10 @@
 BEGIN TRANSACTION;
 
-CREATE TYPE status AS ENUM('fait', 'non');
-
 CREATE TABLE todos (
 	id SERIAL PRIMARY KEY,
 	id_member INT NOT NULL REFERENCES users(id),
 	title VARCHAR(150) NOT NULL,
-	status status NOT NULL DEFAULT 'non',
+	completed BOOL DEFAULT false,
 	created_on TIMESTAMP NOT NULL,
 	done_on TIMESTAMP NULL
 );
