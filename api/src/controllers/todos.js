fbfc16ce0dd5baec569ebb6015341a07b2b0c3c6
Tom687 <tom.pomarede687@gmail.com>
2022-10-14 00:14:49 +0200

    Fixed code to work with 'completed' todos field instead of 'status'

--- api/src/controllers/todos.js
+++ api/src/controllers/todos.js
@@ -6,7 +6,7 @@ import moment from 'moment';
 export const insertTodo = catchAsync(async (req, res, next) => {
 	const { title } = req.body;
 	
-	if (!title || title === 'e')
+	if (!title)
 		return next(new AppError('Il manque des infos pour insérer le todo en DB', 400));
 
 	const [insertedTodo] = await db('todos').insert({
@@ -18,20 +18,20 @@ export const insertTodo = catchAsync(async (req, res, next) => {
 	res.status(201).json({
 		status: 'success',
 		message: 'Todo créé et inséré en DB avec succès',
-		id: insertedTodo.id
-	})
+		id: insertedTodo.id,
+	});
 });
 
 export const getTodos = catchAsync(async (req, res, next) => {
-	const todos = await db('todos').select('id', 'id_member AS memberId', 'title', 'status', 'created_on AS createdOn', 'done_on AS doneOn')
+	const todos = await db('todos').select('id', 'id_member AS memberId', 'title', 'completed', 'created_on AS createdOn', 'done_on AS doneOn')
 		.where({ id_member: res.locals.user.id })
 		.orderBy('id', 'asc');
 	
 	res.status(200).json({
 		status: 'success',
 		todos,
-		message: 'Todolist récupérée avec succès depuis la DB' // FIXME : Modifier / retirer message
-	})
+		message: 'Todolist récupérée avec succès depuis la DB',
+	});
 });
 
 
@@ -41,8 +41,13 @@ export const editTodo = catchAsync(async (req, res, next) => {
 	
 	res.status(200).json({
 		status: 'success',
-		message: 'Todo modifié avec succès'
+		message: 'Todo modifié avec succès',
 	});
+});
+
+export const toggleAllTodos = catchAsync(async (req, res, next) => {
+	await db('todos').where({ id_member: res.locals.user.id })
+		.update({ completed: 1 })
 });
 
 
@@ -51,6 +56,6 @@ export const deleteTodo = catchAsync(async (req, res, next) => {
 	
 	res.status(200).json({
 		status: 'success',
-		message: 'Todo supprimé avec succès'
+		message: 'Todo supprimé avec succès',
 	});
 });
