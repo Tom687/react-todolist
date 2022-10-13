c978dc6ce1ca8469c8e3b882c73fa8cfdbc6fb47
Tom687 <tom.pomarede687@gmail.com>
2022-10-13 23:08:48 +0200

    Cleaned code

--- api/src/utils/appError.js
+++ api/src/utils/appError.js
@@ -3,7 +3,7 @@ class AppError extends Error {
 		super(message);
 		
 		this.statusCode = statusCode;
-		this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error'; // TODO : Pourquoi utiliser ${} et pas direct ?
+		this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
 		this.isOperational = true; // If we should display error on front-end or not
 		
 		Error.captureStackTrace(this, this.constructor); // Keep the error stacktrace
